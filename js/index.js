e3077cf4bfe752982d9ec7787e7ebd76e40e4bcc
scmdesai <scmdesai@gmail.com>
2015-09-19 19:11:13 -0500

    Reverted to PhoneGap previous config

--- js/index.js
+++ js/index.js
@@ -33,45 +33,17 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-	/*
-        var push = PushNotification.init({
-            "android": {
-                "senderID": "226322216862"
-            },
-            "ios": {}, 
-            "windows": {} 
-        });
-        
-        push.on('registration', function(data) {
-            console.log("registration event");
-            document.getElementById("regId").innerHTML = data.registrationId;
-            console.log(JSON.stringify(data));
-        });
-
-        push.on('notification', function(data) {
-        	console.log("notification event");
-            console.log(JSON.stringify(data));
-            var cards = document.getElementById("cards");
-            var push = '<div class="row">' +
-		  		  '<div class="col s12 m6">' +
-				  '  <div class="card darken-1">' +
-				  '    <div class="card-content black-text">' +
-				  '      <span class="card-title black-text">' + data.title + '</span>' +
-				  '      <p>' + data.message + '</p>' +
-				  '    </div>' +
-				  '  </div>' +
-				  ' </div>' +
-				  '</div>';
-            cards.innerHTML += push;
-        });
-
-        push.on('error', function(e) {
-            console.log("push error");
-        });
-	*/	
 		app.receivedEvent('deviceready');
 	},
 	
+	
+    // Handle the back button
+    
+    /*function onBackKeyDown() {
+	alert('Back Button pressed!');
+    }*/
+	
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
