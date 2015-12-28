1aaccfb9065573ceba234e82c2dffb0b114a6494
scmdesai <scmdesai@gmail.com>
2015-12-28 13:59:04 -0600

    Removed Google Analytics nd added AWS Mobile Analytics

--- js/index.js
+++ js/index.js
@@ -71,6 +71,23 @@ var app = {
 			console.log("Error Message is: " + e.message) ;				
         });
 		
+		/* Amazon Mobile Analytics*/
+		AWS.config.region = 'us-east-1';
+		AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+		IdentityPoolId: 'us-east-1:cd2c5cf7-2b6d-49a6-be5f-5d1a92113389' //Amazon Cognito Identity Pool ID
+		
+		var options = {
+    appId : 'b8f01603ccc24b2c9d0e78b76334febe', //Amazon Mobile Analytics App ID
+	platform: 'JavaScript',
+    appTitle : 'Local Buzz',              //Optional e.g. 'Example App'
+    
+};
+
+var mobileAnalyticsClient = new AMA.Manager(options);
+mobileAnalyticsClient.startSession();
+mobileAnalyticsClient.submitEvents();
+
+});		
 	}
 };
 app.initialize();
