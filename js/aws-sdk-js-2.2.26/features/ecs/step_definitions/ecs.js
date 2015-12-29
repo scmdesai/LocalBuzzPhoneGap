06dce7328a85afd29df2cfe64d3962d3b97f0e3c
scmdesai <scmdesai@gmail.com>
2015-12-28 22:03:10 -0600

    Removed all unwanted files Amazon Analytics
    Removed folders aws-sdk-js-master and aws-sdk-js-2.2.26

--- js/aws-sdk-js-2.2.26/features/ecs/step_definitions/ecs.js
+++
@@ -1,8 +0,0 @@
-module.exports = function() {
-  this.Before("@ecs", function (callback) {
-    this.service = new this.AWS.ECS({region: 'us-east-1'});
-    callback();
-  });
-
-  // Add step definitions
-};
