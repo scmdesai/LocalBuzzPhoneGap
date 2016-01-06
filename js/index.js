d2fe115101d159c66cbc5343afbb860264bdde46
scmdesai <scmdesai@gmail.com>
2016-01-06 13:23:03 -0600

    postal code

--- js/index.js
+++ js/index.js
@@ -141,9 +141,9 @@ function showPosition(position)
  
          //separately if you want, throw in the postal code, do another api call
         $.getJSON("http://api.geonames.org/findNearbyPostalCodesJSON?lat=" + latitude + "&lng=" + longitude + "&username=1234_5678", function(json) {
-                       // analytics.trackEvent('set', 'dimension5', postalCodes.toString());
-						console.log(json.postalCodes[0]);
-                        analytics.trackEvent('DealClick','geolocation', postalCodes[0].postalCode.toString());
+                        analytics.trackEvent('set', 'dimension5', json.postalCodes[0].postalCode);
+						console.log(json.postalCodes[0].postalCode);
+                        analytics.trackEvent('DealClick','geolocation', json.postalCodes[0].postalCode);
       });
  
 }
