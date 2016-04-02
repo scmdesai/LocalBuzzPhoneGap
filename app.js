373f97a73703704d2714629e306eaf288da3dfd4
scmdesai <scmdesai@gmail.com>
2016-04-02 00:24:07 -0500

    window create

--- app.js
+++ app.js
@@ -65039,12 +65039,49 @@ Ext.define('Ext.direct.Manager', {
             });*/
             google.maps.event.addListener(marker, 'mousedown', function() {
                 // infoWindow.open(gmap, marker);
-                var storeInfo = Ext.Viewport.add({
-                        xtype: 'contactinfo'
-                    });
-                storeInfo.setRecord(record);
+                //var storeInfo = Ext.Viewport.add({xtype:'contactinfo'});
+                // storeInfo.setRecord(record);
                 //Ext.Viewport.setActiveItem(storeInfo);
-                storeInfo.showBy(marker);
+                win = Ext.create('widget.window', {
+                    closable: true,
+                    closeAction: 'hide',
+                    maximizable: true,
+                    animateTarget: button,
+                    width: 600,
+                    minWidth: 350,
+                    height: 350,
+                    items: [
+                        {
+                            region: 'west',
+                            title: 'Navigation',
+                            width: 200,
+                            split: true,
+                            collapsible: true,
+                            floatable: false
+                        },
+                        {
+                            region: 'center',
+                            xtype: 'tabpanel',
+                            items: [
+                                {
+                                    // LTR even when example is RTL so that the code can be read
+                                    rtl: false,
+                                    title: 'Bogus Tab',
+                                    html: '<p>Window configured with:</p><pre style="margin-left:20px"><code>header: {\n    titlePosition: 2,\n    titleAlign: "center"\n},\nmaximizable: true,\ntools: [{type: "pin"}],\nclosable: true</code></pre>'
+                                },
+                                {
+                                    title: 'Another Tab',
+                                    html: 'Hello world 2'
+                                },
+                                {
+                                    title: 'Closable Tab',
+                                    html: 'Hello world 3',
+                                    closable: true
+                                }
+                            ]
+                        }
+                    ]
+                });
             });
         }
     }
