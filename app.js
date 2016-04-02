03090c3ad446a70869165c2e0f930c72cf7b7d3d
scmdesai <scmdesai@gmail.com>
2016-04-01 23:23:54 -0500

    check control

--- app.js
+++ app.js
@@ -65031,33 +65031,19 @@ Ext.define('Ext.direct.Manager', {
                     icon: icons[feature].icon
                 });
             var content = "<div>" + businessName + "</div><label id=\"labelStore\">Store Info</label>";
-            addInfoWindow(marker, content);
+            addInfoWindow(marker, content, record);
         }
-        function addInfoWindow(marker, content) {
-            var infoWindow = new google.maps.InfoWindow({
-                    content: content
-                });
+        function addInfoWindow(marker, content, record) {
+            /*var infoWindow = new google.maps.InfoWindow({
+                content: content
+            });*/
             google.maps.event.addListener(marker, 'mousedown', function() {
-                infoWindow.open(gmap, marker);
-                google.maps.event.addListener(infoWindow, 'mousedown', function() {
-                    console.log('Info window clicked');
-                    var showStore = Ext.Viewport.add({
-                            xtype: 'contactinfo'
-                        });
-                    //showStore.setRecord(record);
-                    Ext.Viewport.setActiveItem(showStore);
-                });
+                // infoWindow.open(gmap, marker);
+                var view = Ext.Viewport.add({
+                        xtype: 'contactinfo'
+                    });
+                view.setRecord(record);
             });
-            function pinSymbol(color) {
-                return {
-                    path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z',
-                    fillColor: color,
-                    fillOpacity: 1,
-                    strokeColor: '#000',
-                    strokeWeight: 2,
-                    scale: 4
-                };
-            }
         }
     }
 }, 0, 0, [
