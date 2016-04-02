33ecfb0fa9abe25d109be56858b37325a147af5c
scmdesai <scmdesai@gmail.com>
2016-04-02 00:34:40 -0500

    test window

--- app.js
+++ app.js
@@ -65043,42 +65043,17 @@ Ext.define('Ext.direct.Manager', {
                 // storeInfo.setRecord(record);
                 //Ext.Viewport.setActiveItem(storeInfo);
                 win = Ext.create('widget.window', {
+                    title: 'Layout Window',
                     closable: true,
                     closeAction: 'hide',
-                    maximizable: true,
-                    animateTarget: marker,
                     width: 600,
                     minWidth: 350,
                     height: 350,
+                    layout: 'border',
+                    bodyStyle: 'padding: 5px;',
                     items: [
                         {
-                            region: 'west',
-                            title: 'Navigation',
-                            width: 200,
-                            split: true,
-                            collapsible: true,
-                            floatable: false
-                        },
-                        {
-                            region: 'center',
-                            xtype: 'tabpanel',
-                            items: [
-                                {
-                                    // LTR even when example is RTL so that the code can be read
-                                    rtl: false,
-                                    title: 'Bogus Tab',
-                                    html: '<p>Window configured with:</p><pre style="margin-left:20px"><code>header: {\n    titlePosition: 2,\n    titleAlign: "center"\n},\nmaximizable: true,\ntools: [{type: "pin"}],\nclosable: true</code></pre>'
-                                },
-                                {
-                                    title: 'Another Tab',
-                                    html: 'Hello world 2'
-                                },
-                                {
-                                    title: 'Closable Tab',
-                                    html: 'Hello world 3',
-                                    closable: true
-                                }
-                            ]
+                            xtype: 'contactinfo'
                         }
                     ]
                 });
