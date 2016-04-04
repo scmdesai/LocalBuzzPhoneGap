86b1fb553aa5366c3f9cfce86396fee9e633a667
scmdesai <scmdesai@gmail.com>
2016-04-04 14:06:47 -0500

    gmap set option

--- app.js
+++ app.js
@@ -64926,11 +64926,11 @@ Ext.define('Ext.direct.Manager', {
                 $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + postalCode + "&key=AIzaSyDHFtBdpwHNSJ2Pu0HpRK1ce5uHCSGHKXM", function(json) {
                     lat = json.results[0].geometry.location.lat;
                     long = json.results[0].geometry.location.lng;
-                    map.setMapCenter(new google.maps.LatLng({
-                        latitude: lat,
-                        longitude: long
-                    }));
                 });
+                gmap.setMapCenter(new google.maps.LatLng({
+                    latitude: lat,
+                    longitude: long
+                }));
             });
         }
         var store = Ext.getStore('MyJsonPStore');
