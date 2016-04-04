52e6731f2eba7a6105522c0455eebbf3cc9546c4
scmdesai <scmdesai@gmail.com>
2016-04-04 10:49:27 -0500

    center map

--- app.js
+++ app.js
@@ -65066,7 +65066,7 @@ Ext.define('Ext.direct.Manager', {
         }
     },
     onMymapCenterChange: function(map, gmap, center, eOpts) {
-        gmap.setData(center);
+        gmap.setMapCenter(center);
     }
 }, 0, [
     "Main"
