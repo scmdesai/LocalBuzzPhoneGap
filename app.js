08b9032e8265206214076371fcb9e04076b1e825
scmdesai <scmdesai@gmail.com>
2016-04-15 22:41:18 -0500

    dealBackButton

--- app.js
+++ app.js
@@ -65215,7 +65215,7 @@ Ext.define('Ext.direct.Manager', {
             function onBackKeyDown(eve) {
                 eve.preventDefault();
                 // Ext.Viewport.getActiveItem().destroy();
-                Ext.getCmp('dealBackBtn').fireEvent('tap');
+                Ext.getCmp('dealBackButton').fireEvent('click');
             }
         }
     }
