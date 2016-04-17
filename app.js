71b3121ba8e945c1da5020c88356552ed4e0c077
scmdesai <scmdesai@gmail.com>
2016-04-17 02:49:05 -0500

    active tab

--- app.js
+++ app.js
@@ -66489,7 +66489,7 @@ Ext.application({
                     if (tabPanel.getItemId() === 'LatestBuzz') {
                         navigator.app.exitApp();
                     } else {
-                        tabPanel.setActiveItem(tabPanel.down('#LatestBuzz'));
+                        tabPanel.setActiveItem(tabPanel.getAt(0));
                     }
                 } else if (Ext.Viewport.getActiveItem().getItemId() === 'Info') {
                     Ext.Viewport.getActiveItem().destroy();
