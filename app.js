47b6e43ce0f51a068a82a95f9bb4004265c965ca
scmdesai <scmdesai@gmail.com>
2016-04-22 15:10:07 -0500

    store

--- app.js
+++ app.js
@@ -64902,6 +64902,7 @@ Ext.define('Ext.direct.Manager', {
                                     $.getJSON("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + latitude + "," + longitude + "&destinations=" + address + "&key=AIzaSyDHFtBdpwHNSJ2Pu0HpRK1ce5uHCSGHKXM", function(json) {
                                         var distance = json.rows[0].elements[0].distance.value;
                                         if (distance <= 1610) {
+                                            console.log(customerId);
                                             storesNearBy.add({
                                                 'customerId': customerId
                                             });
