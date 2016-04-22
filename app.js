815bfaa550174fdf1274745c8a8aff4c43551112
scmdesai <scmdesai@gmail.com>
2016-04-21 21:47:25 -0500

    listener

--- app.js
+++ app.js
@@ -65117,6 +65117,9 @@ Ext.define('Ext.direct.Manager', {
                 });
             var content = '<h4 id ="businessname">' + businessName + '</h4><div><label id="labelStore" style="color:green;font-size:4vw;text-decoration:underline">' + count + ' Active Buzz</label></div>';
             addInfoWindow(marker, content, record, businessName);
+            google.maps.event.addListener(gmap, 'tilesloaded', function() {
+                console.log('Tiles loaded');
+            });
         }
         function addInfoWindow(marker, content, record, businessName) {
             /*infoWindow = new google.maps.InfoWindow({
