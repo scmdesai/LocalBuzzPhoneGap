ebcb71f9ae828c1ca90eaff55fa425bddfb2cc27
scmdesai <scmdesai@gmail.com>
2016-04-22 22:07:29 -0500

    store length

--- app.js
+++ app.js
@@ -64730,7 +64730,6 @@ Ext.define('Ext.direct.Manager', {
                         storesNearBy.add({
                             'customerId': record.get('customerId')
                         });
-                        console.log(storesNearBy.length);
                         return true;
                     } else {
                         return false;
