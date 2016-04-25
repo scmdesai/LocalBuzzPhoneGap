cc0445412d6b2235fafb85f0c8bd98458320064e
scmdesai <scmdesai@gmail.com>
2016-04-25 09:49:54 -0500

    increased the distance to incluse all deals

--- app.js
+++ app.js
@@ -66793,7 +66793,7 @@ Ext.application({
                 var customerId;
                 $.getJSON("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + latitude + "," + longitude + "&destinations=" + address + "&key=AIzaSyDHFtBdpwHNSJ2Pu0HpRK1ce5uHCSGHKXM", function(json) {
                     var distance = json.rows[0].elements[0].distance.value;
-                    if (distance <= 1000) {
+                    if (distance <= 10000) {
                         storesNearBy.add({
                             'customerId': record.get('customerId')
                         });
