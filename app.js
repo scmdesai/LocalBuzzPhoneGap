8380652e4113c4ad6fdd474d07f01795e536fe95
scmdesai <scmdesai@gmail.com>
2016-04-29 21:39:37 -0500

    label placement

--- app.js
+++ app.js
@@ -66495,7 +66495,7 @@ Ext.define('Ext.direct.Manager', {
                     animation: google.maps.Animation.DROP,
                     icon: icons[category].icon,
                     labelContent: count.toString(),
-                    labelAnchor: new google.maps.Point(-22, 50),
+                    labelAnchor: new google.maps.Point(-15, 50),
                     labelClass: "labels",
                     labelStyle: {
                         opacity: 0.75
