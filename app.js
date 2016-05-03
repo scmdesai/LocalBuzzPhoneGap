8b6369ceeb9af7acbbea62568fe6bdf3629ef942
scmdesai <scmdesai@gmail.com>
2016-05-03 18:50:43 -0500

    target id

--- app.js
+++ app.js
@@ -65930,7 +65930,7 @@ Ext.define('Ext.direct.Manager', {
     },
     onLatestbuzzItemTap: function(dataview, index, target, record, e, eOpts) {
         if (e.target.id === 'favDeal') {
-            console.log(target.getItemId());
+            console.log(target.id);
         } else {
             var pic = Ext.Viewport.add({
                     xtype: 'dealpicture'
