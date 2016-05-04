6c4b74694fbe9a111cabbc32b8ff2b86636b8e1c
scmdesai <scmdesai@gmail.com>
2016-05-04 11:11:32 -0500

    replaceCls

--- app.js
+++ app.js
@@ -66124,7 +66124,7 @@ Ext.define('Ext.direct.Manager', {
     },
     onLatestbuzzItemTap: function(dataview, index, target, record, e, eOpts) {
         if (e.target.id === 'favDeal') {
-            dataview.down('#favDeal').setCls('fill-heart');
+            target.replaceCls('fill-heart');
         } else {
             var pic = Ext.Viewport.add({
                     xtype: 'dealpicture'
