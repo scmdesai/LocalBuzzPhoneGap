7088f88d58ce179defcfd161436fba1c62bcf294
scmdesai <scmdesai@gmail.com>
2016-05-04 18:15:24 -0500

    cls

--- app.js
+++ app.js
@@ -66126,7 +66126,7 @@ Ext.define('Ext.direct.Manager', {
         if (e.target.id === 'favDealBtn') {
             var el = Ext.get('favDealBtn');
             //el.getAttribute('class').replace('fill-heart');
-            el.hide();
+            el.setHTML('Test');
         }
         var pic = Ext.Viewport.add({
                 xtype: 'dealpicture'
