5dc9ba6b352c387ffed56292337eb7c77d2b0f9a
scmdesai <scmdesai@gmail.com>
2016-05-05 00:25:52 -0500

    cls

--- app.js
+++ app.js
@@ -66125,7 +66125,7 @@ Ext.define('Ext.direct.Manager', {
     onLatestbuzzItemTap: function(dataview, index, target, record, e, eOpts) {
         if (e.target.id === 'favDealBtn') {
             var el = e.target.element;
-            el.dom.style.backgroundColor = 'red';
+            el.setCls('fill-heart');
         }
         var pic = Ext.Viewport.add({
                 xtype: 'dealpicture'
