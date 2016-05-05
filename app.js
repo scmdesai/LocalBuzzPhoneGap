5fcd593291f285bbb4e9124d909e0482abee442e
scmdesai <scmdesai@gmail.com>
2016-05-05 18:51:04 -0500

    fill-heart

--- app.js
+++ app.js
@@ -66118,7 +66118,7 @@ Ext.define('Ext.direct.Manager', {
     },
     onLatestbuzzItemTap: function(dataview, index, target, record, e, eOpts) {
         if (e.target.id === 'btn') {
-            index.toggleCls('fill-heart');
+            dataview.getAt(index).setTpl('<button class="fill-heart"></button>');
         }
         var pic = Ext.Viewport.add({
                 xtype: 'dealpicture'
