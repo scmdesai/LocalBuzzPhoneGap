98347772d2e23a3bab42292bc53a70447e7086a0
scmdesai <scmdesai@gmail.com>
2016-05-05 22:52:45 -0500

    share

--- app.js
+++ app.js
@@ -65939,7 +65939,7 @@ Ext.define('Ext.direct.Manager', {
         var record = Ext.getStore('LocalStore').getAt(0);
         //console.log(businessName.customerId);
         //window.plugins.socialsharing.share('Hi!Check out the latest deal from ' + record.get('businessName')+'\n'+record.get('dealName') + '\n' +record.get('dealDescription')+ '\nValid through' +record.get('dealEndDate'),null,record.get('dealDescription'),null );
-        window.plugins.socialsharing.share(null, null, record.get('dealPictureURL'), null);
+        window.plugins.socialsharing.share(record.get('dealName'), null, null, null);
     },
     onDealBackBtn1Tap: function(button, e, eOpts) {
         Ext.Viewport.getActiveItem().destroy();
