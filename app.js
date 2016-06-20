e7e8317c26695b20cf69202fd379dac6adc131f2
scmdesai <scmdesai@gmail.com>
2016-06-20 12:26:01 -0500

    store length

--- app.js
+++ app.js
@@ -66828,7 +66828,7 @@ Ext.define('Ext.direct.Manager', {
                     //stores.push(record.get('customerId'));
                     Ext.Array.include(stores, record.get('customerId'));
                 });
-                console.log(stores.length);
+                Ext.Msg.alert(stores.length, null, null, null);
                 store.filterBy(function(record) {
                     return Ext.Array.indexOf(stores, record.get('customerId')) !== -1;
                 }, this);
