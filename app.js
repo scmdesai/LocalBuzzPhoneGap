81c9c4fe47442e8df3536201bd0e37e7f506418c
scmdesai <scmdesai@gmail.com>
2016-06-29 12:58:25 -0500

    html

--- app.js
+++ app.js
@@ -64905,7 +64905,7 @@ Ext.define('Ext.direct.Manager', {
                 disabled: true,
                 docked: 'bottom',
                 height: '',
-                html: '<u onclick="showStoreDetails()">Click here for store info ',
+                html: '<button onclick="showStoreDetails()">Click here for store info </button>',
                 id: 'nameTxt2',
                 itemId: 'nameTxt2',
                 margin: '5 0 5 0',
