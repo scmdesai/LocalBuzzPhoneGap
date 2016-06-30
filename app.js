82988bcdd8d7235b5d866d733e264d06713a6878
scmdesai <scmdesai@gmail.com>
2016-06-30 13:16:35 -0500

    html

--- app.js
+++ app.js
@@ -64973,7 +64973,6 @@ Ext.define('Ext.direct.Manager', {
             {
                 xtype: 'textareafield',
                 baseCls: 'customfield',
-                cls: 'icon-location',
                 disabled: false,
                 docked: 'bottom',
                 height: '15vh',
@@ -64981,7 +64980,7 @@ Ext.define('Ext.direct.Manager', {
                 id: 'address1',
                 itemId: 'address1',
                 margin: '0 15 0 15',
-                style: 'font-size:3vw;font-family:Arial;text-align: left',
+                style: 'font-size:3.5vw;font-family:Arial;text-align: left',
                 styleHtmlContent: true,
                 top: '70%',
                 width: '90%',
