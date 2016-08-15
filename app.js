bc5c88c95561186ebe0e501e223941b0a5b3866f
scmdesai <scmdesai@gmail.com>
2016-08-14 22:44:41 -0500

    Revert "store"
    This reverts commit b5c4416076e1f0125b5b55b2a00b8a6ee974676c.

--- app.js
+++ app.js
@@ -64750,7 +64750,6 @@ Ext.define('Ext.direct.Manager', {
                     navigator.geolocation.getCurrentPosition(function showPosition(position) {
                         latitude = position.coords.latitude;
                         longitude = position.coords.longitude;
-                        console.log(latitude + "," + longitude);
                         var store = Ext.getStore('MyDealsStore');
                         var stores = [];
                         var storesNearBy = Ext.getStore('calculateDistances');
