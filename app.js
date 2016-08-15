4e9a10df70d2711ae7ec88607d37933032f975ef
scmdesai <scmdesai@gmail.com>
2016-08-14 22:44:16 -0500

    Revert "latitude longitude"
    This reverts commit 263852d493d13b95a72ebe98b5bd927d5314aa17.

--- app.js
+++ app.js
@@ -66882,7 +66882,6 @@ Ext.define('Ext.direct.Manager', {
         if (userLocationStore.getCount() !== 0) {
             latitude = userLocationStore.get('latitude');
             longitude = userLocationStore.get('longitude');
-            console.log(latitude + ',' + longitude);
             Ext.getCmp('mymap').setMapCenter({
                 latitude: latitude,
                 longitude: longitude
